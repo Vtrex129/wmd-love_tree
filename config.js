7866c383a40260d4d9afb550cccfb3d9197bd40c
Vtrex129 <hoangphongvuong2020@gmail.com>
2026-03-08 03:04:19 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+// 霸都丶傲天 2019/7/9  https://Github.com/AJLoveChina
+var config = {
+    // 下面的句子不一定非要7句， 你也可以改成4句，2句话都可以
+    lines: [
+        "Chúc chị một ngày 8/3 thật nhiều niềm vui.",
+        "Luôn xinh đẹp, hạnh phúc và gặp nhiều may mắn.",
+        "Mong rằng chị luôn giữ được sự tự tin, bản lĩnh để theo đuổi những điều mình chọn. ",
+        "Hy vọng những điều tốt đẹp sẽ luôn tìm đến,",
+        "Theo cách nhẹ nhàng nhất.",
+    ],
+    // 相爱的时间 记住格式不能写错了, 非常重要
+    // 格式一定要和下面这个时间格式一样
+    // 分别是 年-月-日T时时:分分:秒秒
+    //      温馨警告:
+    //          1.时分秒都必须是俩个数字  比如 00:00:00
+    //          2.T不能丢了
+    date: "2017-07-20T20:15:00",
+    // 双方的名字
+    names: ["笨笨熊", "黑猪猪"]
+}
